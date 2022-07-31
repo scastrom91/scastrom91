e12add3ecff15b4064026fa264dccf0c3c07d118
scastrom91 <105215785+scastrom91@users.noreply.github.com>
2022-08-01 01:09:47 +0200

    1.1

--- S.Castro - Práctica 1.1 - Modern Exploration/Práctica1.1/main.js
+++ S.Castro - Práctica 1.1 - Modern Exploration/Práctica1.1/main.js
@@ -4,15 +4,13 @@ const height = 600
 const margin = { top: 30, bottom: 20, left: 57, right: 10, info:20}
 let data2
 
-
-
 // Creation of svg & groups
 const svg = d3.select("#chart").append("svg").attr("width", width).attr("height", height)
 
 // Creation of constants & Axis data
 const elementGroup = svg.append("g").attr("id", "elementGroup").attr("transform", `translate(${margin.left}, 0)`)
 const axisGroup = svg.append("g").attr("id", "axisGroup") 
- const xAxisGroup = axisGroup.append("g").attr("id", "xAxisGroup").attr("transform", `translate(${margin.left}, ${height-margin.bottom-margin.top})`)
+const xAxisGroup = axisGroup.append("g").attr("id", "xAxisGroup").attr("transform", `translate(${margin.left}, ${height-margin.bottom-margin.top})`)
 const yAxisGroup = axisGroup.append("g").attr("id", "yAxisGroup").attr("transform", `translate(${margin.left-5}, ${0})`)
 
 // Declaring Scale
@@ -25,43 +23,40 @@ const yAxis = d3.axisLeft().scale(y)
 
 // Now we need the range of domain for the graphic
 d3.csv("data.csv").then(data  => {
-    data.map(d => {d.titles = +d.titles}) //Convert to string
-    data2 = data
+   
+    //Nest
+    data2 = d3.nest()
+    .key(function(d) {
+            return d.winner; 
+        })
+        .sortKeys(d3.ascending)
+        .entries(data)
 
-    // filter empty data on winner "name"
-    //data2 = data2.filter(datanull => datanull != null)
-
-    // // Conversion winner "name" to "times it appears"
-    // var groupedData = d3.nest()
-    // .key(function (d) { return d.manufactured; })
-
-    // // sorting keys in ascending order
-    // .sortKeys(d3.ascending)
-    // .entries(cars);
-    // console.log("ArrayData :", groupedData);
+    //Nulls Filter
+    data2 = data2.filter(d => d.key != "")
 
 
-
-
-    x.domain([0, d3.max(data.map(d => d.titles))])  //Determine range of titles
-    y.domain(data.map(d => d.country))              //Determine countries
+    x.domain([0, d3.max(data2.map(d => d.values.length))])  //Determine range of titles
+    y.domain(data2.map(d => d.key))              //Determine countries
     xAxisGroup.call(xAxis)
     yAxisGroup.call(yAxis)
-    
+    data2.sort(function(a, b){
+        return d3.ascending(a.values)})
     // Data Binding 
     // Crating graphics & axis
-    elementGroup.selectAll("rect").data(data)    
+    elementGroup.selectAll("rect").data(data2)    
         .join("rect")
-            .attr("class", d => d.country)
-            .attr("x", 0)
-            .attr("y", d => y(d.country))
-            .attr("width", d => x(d.titles))
+            .attr("class", d => d.key)
             .attr("height", y.bandwidth())
+            .attr("x", 0)
+            .attr("y", d => y(d.key)) 
+            .attr("width", d => x(d.values.length))
 
     // Creating text inside graphics
-    elementGroup.selectAll("text").data(data)
-        .join("text")
-            .text(d => d.titles)
-            .attr("x", d=> x(d.titles)+3)
-            .attr("y", d=> y(d.country)+35)
+    
+    // elementGroup.selectAll("text").data(data2)
+    //     .join("text")
+    //         .text(d => d.values.length)
+    //         .attr("x", d => x(d.values)+33)
+    //         .attr("y", d => y(d.key)+35)
 })
